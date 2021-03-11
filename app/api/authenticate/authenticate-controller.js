45fb5e75407e9c112883c5f7e27d8d726643b107
Leonardo Furnielis <leonardofurnielis@outlook.com>
2021-03-11 12:17:02 -0300

    updates on eslint and add travis ci

--- app/api/authenticate/authenticate-controller.js
+++ app/api/authenticate/authenticate-controller.js
@@ -3,14 +3,14 @@
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
-const FindByCredentials = require('../../guards/user-authenticate');
+const findByCredentials = require('../../guards/user-authenticate');
 
 const authenticate = async (req, res, next) => {
   try {
     const base64Auth = (req.headers.authorization || '').split(' ')[1] || '';
     const [username, password] = Buffer.from(base64Auth, 'base64').toString().split(':');
 
-    const user = await FindByCredentials(username, password);
+    const user = await findByCredentials(username, password);
     const payload = _.pick(user, ['_id', 'username', 'name', 'email', 'active']);
 
     const token = jwt.sign(payload, process.env.PW_SECRET, {
